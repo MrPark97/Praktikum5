4c0d82201ccaa0b81dc298552adb962219c2d024
MrPark97 <evgeniy.pak.97@mail.ru>
2019-01-16 19:55:31 +0500

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Praktikum5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Praktikum5.dir/main.cpp.obj"
+  "Praktikum5.pdb"
+  "Praktikum5.exe"
+  "Praktikum5.exe.manifest"
+  "libPraktikum5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Praktikum5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Praktikum5.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Praktikum5.
+# This may be replaced when dependencies are built.
